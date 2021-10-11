c451c8f7e1976e367b6486f0b02a2b127f038007
Fabio Matos <fabio.thunder@hotmail.com>
2021-10-11 18:46:42 -0300

    estudando estrutura for

---
+++ CursoNelio/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CursoNelioCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite um número maior que 1 e menor que 1000");
+            int x = int.Parse(Console.ReadLine());
+            if( 1<= x && x <= 1000)
+            {          
+                    for(int i=1; i <=x; i+=2)
+                    {
+                    Console.WriteLine(i);
+                    }
+            }
+            else
+            {
+                Console.WriteLine("STACKOVERFLOW");
+            }
+        }
+    }
+}
