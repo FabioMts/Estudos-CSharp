48275bfdec2bc31b90d5b246e9a1f32398ce6db6
Fabio Matos <fabio.thunder@hotmail.com>
2021-10-11 19:26:03 -0300

    Exercicios estrutura for

---
+++ CursoNelio/CursoNelioCSharp/CursoNelioCSharp/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace CursoNelioCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*  Console.WriteLine("Digite um número maior que 1 e menor que 1000");
+              int x = int.Parse(Console.ReadLine());
+              if( 1<= x && x <= 1000)
+              {          
+                      for(int i=1; i <=x; i+=2)
+                      {
+                      Console.WriteLine(i);
+                      }
+              }
+              else
+              {
+                  Console.WriteLine("STACKOVERFLOW");
+              }
+  */
+
+            // EXERCICIO 2
+
+
+           /* Console.WriteLine("Quantos números inteiros você vai digitar? ");
+            int N = int.Parse(Console.ReadLine());
+
+            int In = 0;
+            int Out = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                int valor = int.Parse(Console.ReadLine());
+                if (valor >= 10 && valor <= 20)
+                {
+                    In++;
+                }
+                else
+                {
+                    Out++;
+                }
+               
+            }
+            Console.WriteLine("In: " + In);
+            Console.WriteLine("Out: " + Out);*/
+
+
+
+            // EXERCICIO 3
+
+        }
+    }
+}
