1947ef0624ac7ca656e1b5b13540487e0294d1c9
Fabio Matos <fabio.thunder@hotmail.com>
2021-10-12 12:30:15 -0300

    exercitando classes atributos e metodos

---
+++ CursoNelio/CursoNelioCSharp/EXE02/Funcionario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE02
+{
+    class Funcionario
+    {
+        public String Nome;
+        public double SalarioBruto;
+        public double Imposto = 1000;
+
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * (porcentagem / 100));
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", R$ "
+                + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ CursoNelio/CursoNelioCSharp/EXE02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EXE02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Funcionario f = new Funcionario();
+
+
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+
+            Console.Write("Salario bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Imposto: " + f.Imposto);
+            Console.WriteLine();
+            Console.WriteLine(f);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Deseja aumentar o salário em qual porcentagem: ");
+            double porcent = double.Parse(Console.ReadLine());
+            f.AumentarSalario(porcent);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atualizados: ");
+            Console.WriteLine(f);
+        }
+    }
+}
